16e124110f3b5cb111ad600ed7334b9a373c1e6a
SharpsBrain <109470818+SharpsBrain@users.noreply.github.com>
2022-09-13 10:06:45 +0500

    Add files via upload

---
+++ Untitled40.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+mytuple=("apple","banana","cherry","apple","banana","cherry")
+print(len(mytuple))
+
+
+# In[4]:
+
+
+mytuple=("apple",)
+print(type(mytuple))
+
+#not a tuple:
+mytuple=("applle")
+print(type(mytuple))
+
+
+# In[5]:
+
+
+tuple1=("apple","banana","cherry")
+tuple2=(1,4,5,6,7)
+tuple3=(True,False,True)
+print(tuple1,tuple2,tuple3)
+
+
+# In[6]:
+
+
+tuple1=("sharps",123,True,23,"brain")
+print(tuple1)
+
+
+# In[7]:
+
+
+mytuple=("apple",)
+print(type(mytuple))
+
+#not a tuple:
+mytuple=("applle")
+print(type(mytuple))
+
+
+# In[8]:
+
+
+mytuple=tuple(("apple","banana"))
+print(mytuple)
+
+
+# In[ ]:
+
+
+
+
